5e4ce21d1ee8338e1d5eefa1ca5820d0689a6600
Emil Albrektsson <emil.albrektsson@nav.no>
2020-09-10 11:55:59 +0200

    Cleanup debug

--- src/main/kotlin/no/nav/omsorgspenger/App.kt
+++ src/main/kotlin/no/nav/omsorgspenger/App.kt
@@ -3,29 +3,11 @@ package no.nav.omsorgspenger
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.LoggerFactory
-import java.io.File
-import java.io.FileNotFoundException
 
 internal val logger = LoggerFactory.getLogger("no.nav.omsorgspenger")
 
 fun main() {
     var env = System.getenv()
-
-    if(env.containsKey("username") && env.containsKey("password")) {
-        logger.info("Contains username & password!")
-    }
-
-    val user = "/var/run/secrets/nais.io/service_user/username".readFile()
-    val pass = "/var/run/secrets/nais.io/service_user/password".readFile()
-
-    if(user != null ) {
-        logger.info("Username found: $user")
-    }
-
-    if(pass != null) {
-        logger.info("Password found")
-    }
-
 
     RapidApplication.create(env).apply {
         OmsorgspengerRammemeldinger(this)
@@ -37,10 +19,3 @@ fun main() {
         })
     }.start()
 }
-
-internal fun String.readFile() =
-        try {
-            File(this).readText(Charsets.UTF_8)
-        } catch (err: FileNotFoundException) {
-            logger.error("File not found: $this")
-        }
