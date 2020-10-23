036e9d72040ed08cbce50db4c4d6efd129df76b0
Anders Kristiansen <anders.kristiansen@nav.no>
2020-10-23 13:07:28 +0200

    skriver om beregningsfunksjon

--- src/test/kotlin/no/nav/omsorgspenger/overføringer/BeregningerTest.kt
+++ src/test/kotlin/no/nav/omsorgspenger/overføringer/BeregningerTest.kt
@@ -1,102 +1,168 @@
 package no.nav.omsorgspenger.overføringer
 
+import no.nav.omsorgspenger.Periode
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 internal class BeregningerTest {
 
     @Test
     internal fun `1 til 2 barn gir 10 dager`() {
+        val nå = LocalDate.now()
+        val etBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = false,
+            utvidetRett = false,
+        )
+
+        val periode = Periode(nå, nå)
+
         for (antallBarn in 1..2) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = antallBarn,
-                antallBarnMedAleneOmOmsorgen = 0,
-                antallBarnMedUtvidetRett = 0,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = 0
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = 1.rangeTo(antallBarn).map { etBarn },
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 10)
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 10)
         }
     }
 
     @Test
     internal fun `3 eller fler barn gir 15 dager`() {
+        val nå = LocalDate.now()
+        val etBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = false,
+            utvidetRett = false,
+        )
+
+        val periode = Periode(nå, nå)
+
         for (antallBarn in 3..23 step 5) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = antallBarn,
-                antallBarnMedAleneOmOmsorgen = 0,
-                antallBarnMedUtvidetRett = 0,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = 0
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = 1.rangeTo(antallBarn).map { etBarn },
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 15)
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 15)
         }
     }
 
     @Test
     internal fun `1 til 2 barn med aleneomsorg for, gir 10 ekstra dager`() {
+        val nå = LocalDate.now()
+        val etBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = true,
+            utvidetRett = false,
+        )
+
+        val periode = Periode(nå, nå)
+
         for (antallBarn in 1..2) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = 2,
-                antallBarnMedAleneOmOmsorgen = antallBarn,
-                antallBarnMedUtvidetRett = 0,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = 0
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = 1.rangeTo(antallBarn).map { etBarn },
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 20)
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 20)
         }
     }
 
     @Test
     internal fun `3 eller fler barn med aleneomsorg for, gir 15 ekstra dager`() {
+        val nå = LocalDate.now()
+        val etBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = true,
+            utvidetRett = false,
+        )
+
+        val periode = Periode(nå, nå)
+
         for (antallBarn in 3..23 step 5) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = antallBarn,
-                antallBarnMedAleneOmOmsorgen = antallBarn,
-                antallBarnMedUtvidetRett = 0,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = 0
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = 1.rangeTo(antallBarn).map { etBarn },
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 30)
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 30)
         }
     }
 
     @Test
     internal fun `Får 10 ekstra dager per barn med utvidet rett`() {
+        val nå = LocalDate.now()
+        val vanligBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = false,
+            utvidetRett = false,
+        )
+
+        val vanligeBarn = 1.rangeTo(3).map { vanligBarn }
+
+        val utvidetRettBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = false,
+            utvidetRett = true,
+        )
+
+        val periode = Periode(nå, nå)
+
         for (antallUtvidetRett in 1..5) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = 5,
-                antallBarnMedAleneOmOmsorgen = 0,
-                antallBarnMedUtvidetRett = antallUtvidetRett,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = 0
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = vanligeBarn.plus(1.rangeTo(antallUtvidetRett).map { utvidetRettBarn }),
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 15 + (10 * antallUtvidetRett))
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 15 + (10 * antallUtvidetRett))
         }
     }
 
     @Test
     internal fun `Får 20 ekstra dager per barn med utvidet rett og aleneomsorg, i tillegg til vanlig aleneomsorgsdager`() {
-        for (antallUtvidetOgAlene in 0..2) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = 2,
-                antallBarnMedAleneOmOmsorgen = 2,
-                antallBarnMedUtvidetRett = 0,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = antallUtvidetOgAlene
+        val nå = LocalDate.now()
+        val aleneOgUtvidetRettBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = true,
+            utvidetRett = true,
+        )
+
+        val periode = Periode(nå, nå)
+
+        for (antallUtvidetOgAlene in 1..2) {
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = 1.rangeTo(antallUtvidetOgAlene).map { aleneOgUtvidetRettBarn },
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 20 + (20 * antallUtvidetOgAlene))
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 20 + (20 * antallUtvidetOgAlene))
         }
 
-        for (antallUtvidetOgAlene in 3..23) {
-            val omsorgsdager = Beregninger.beregnOmsorgsdager(
-                antallBarnMedOmsorgenFor = 30,
-                antallBarnMedAleneOmOmsorgen = 15,
-                antallBarnMedUtvidetRett = 0,
-                antallBarnMedUtvidetRettOgAleneOmOmsorgen = antallUtvidetOgAlene
+        val vanligBarn = Barn(
+            identitetsnummer = "",
+            fødselsdato = nå.minusYears(1),
+            aleneOmOmsorgen = false,
+            utvidetRett = false,
+        )
+
+        val vanligeBarn = 1.rangeTo(3).map { vanligBarn }
+
+        for (antallUtvidetOgAlene in 3..23 step 5) {
+            val omsorgsdagerResultat = Beregninger.beregnOmsorgsdager(
+                barn = vanligeBarn.plus(1.rangeTo(antallUtvidetOgAlene).map { aleneOgUtvidetRettBarn }),
+                periode = periode
             )
 
-            assertEquals(omsorgsdager, 30 + (20 * antallUtvidetOgAlene))
+            assertEquals(omsorgsdagerResultat.antallOmsorgsdager(), 30 + (20 * antallUtvidetOgAlene))
         }
     }
 }
