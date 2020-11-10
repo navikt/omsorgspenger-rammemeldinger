075b294eb71bbeabae19da21459d905b9009d24b
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-11-10 16:29:45 +0100

    Test på toLocalDateOslo

--- src/main/kotlin/no/nav/omsorgspenger/extensions/LocalDateExt.kt
+++ src/main/kotlin/no/nav/omsorgspenger/extensions/LocalDateExt.kt
@@ -8,4 +8,4 @@ private val Oslo = ZoneId.of("Europe/Oslo")
 internal fun LocalDate.erFørEllerLik(annen: LocalDate) = isBefore(annen) || isEqual(annen)
 internal fun LocalDate.førsteDagNesteÅr() = withYear(year+1).withMonth(1).withDayOfMonth(1)
 internal fun LocalDate.sisteDagIÅret() = withMonth(12).withDayOfMonth(31)
-internal fun ZonedDateTime.toLocalDateOslo() = withZoneSameInstant(Oslo).toLocalDate() // TODO: Test
+internal fun ZonedDateTime.toLocalDateOslo() = withZoneSameInstant(Oslo).toLocalDate()
---
+++ src/test/kotlin/no/nav/omsorgspenger/extensions/LocalDateExtTest.kt
@@ -0,0 +1,21 @@
+package no.nav.omsorgspenger.extensions
+
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import kotlin.test.assertEquals
+
+internal class LocalDateExtTest {
+
+    @Test
+    fun `Test toLocalDateOslo i overgangen til ny dag`() {
+        val zonedDateTime = ZonedDateTime.parse("2020-11-10T23:00:00.00Z")
+        assertEquals(LocalDate.parse("2020-11-11"), zonedDateTime.toLocalDateOslo())
+    }
+
+    @Test
+    fun `Test toLocalDateOslo midt på dagen`() {
+        val zonedDateTime = ZonedDateTime.parse("2020-11-10T12:00:00.00Z")
+        assertEquals(LocalDate.parse("2020-11-10"), zonedDateTime.toLocalDateOslo())
+    }
+}
