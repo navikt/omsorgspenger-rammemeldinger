e9f41eae4cd3a5e4b50eb0c7497a47973b2d320e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-11-14 13:51:12 +0100

    Flytter  ApplicationContext inn i egen fil.

--- src/main/kotlin/no/nav/omsorgspenger/App.kt
+++ src/main/kotlin/no/nav/omsorgspenger/App.kt
@@ -8,28 +8,12 @@ import io.ktor.features.*
 import io.ktor.jackson.*
 import io.ktor.routing.*
 import no.nav.helse.dusseldorf.ktor.health.HealthRoute
-import no.nav.helse.dusseldorf.ktor.health.HealthService
-import no.nav.helse.dusseldorf.oauth2.client.AccessTokenClient
-import no.nav.helse.dusseldorf.oauth2.client.ClientSecretAccessTokenClient
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
-import no.nav.k9.rapid.river.Environment
-import no.nav.k9.rapid.river.KafkaBuilder.kafkaProducer
-import no.nav.k9.rapid.river.csvTilSet
-import no.nav.k9.rapid.river.hentRequiredEnv
-import no.nav.omsorgspenger.fordelinger.FordelingService
-import no.nav.omsorgspenger.formidling.FormidlingService
-import no.nav.omsorgspenger.infotrygd.InfotrygdRammeService
-import no.nav.omsorgspenger.infotrygd.OmsorgspengerInfotrygdRammevedtakGateway
-import no.nav.omsorgspenger.midlertidigalene.MidlertidigAleneService
-import no.nav.omsorgspenger.overføringer.GjennomførOverføringService
 import no.nav.omsorgspenger.overføringer.OverføringerApi
 import no.nav.omsorgspenger.overføringer.rivers.PubliserOverføringAvOmsorgsdager
 import no.nav.omsorgspenger.overføringer.rivers.BehandleOverføringAvOmsorgsdager
 import no.nav.omsorgspenger.overføringer.rivers.InitierOverføringAvOmsorgsdager
-import no.nav.omsorgspenger.utvidetrett.UtvidetRettService
-import org.apache.kafka.clients.producer.KafkaProducer
-import java.net.URI
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.auth.*
 import io.ktor.http.*
@@ -47,13 +31,6 @@ import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.common.TextFormat
 import no.nav.helse.dusseldorf.ktor.auth.*
 import no.nav.omsorgspenger.aleneom.AleneOmOmsorgenApi
-import no.nav.omsorgspenger.aleneom.AleneOmOmsorgenRepository
-import no.nav.omsorgspenger.overføringer.OverføringRepository
-import no.nav.omsorgspenger.saksnummer.SaksnummerRepository
-import javax.sql.DataSource
-import no.nav.omsorgspenger.aleneom.AleneOmOmsorgenService
-import no.nav.omsorgspenger.overføringer.OverføringService
-import no.nav.omsorgspenger.saksnummer.SaksnummerService
 
 fun main() = when (System.getenv("RAPIDS_APPLICATION") == "disabled") {
     true -> ktorApplication()
@@ -162,131 +139,6 @@ internal fun Application.omsorgspengerRammemeldinger(applicationContext: Applica
         authenticate(*accessAsApplicationIssuers.allIssuers()) {
             OverføringerApi(overføringService = applicationContext.overføringService)
             AleneOmOmsorgenApi(aleneOmOmsorgenService = applicationContext.aleneOmOmsorgenService)
-        }
-    }
-}
-
-internal class ApplicationContext(
-    internal val env: Environment,
-    internal val accessTokenClient: AccessTokenClient,
-    internal val omsorgspengerInfotrygdRammevedtakGateway: OmsorgspengerInfotrygdRammevedtakGateway,
-    internal val infotrygdRammeService: InfotrygdRammeService,
-    internal val fordelingService: FordelingService,
-    internal val utvidetRettService: UtvidetRettService,
-    internal val midlertidigAleneService: MidlertidigAleneService,
-    internal val gjennomførOverføringService: GjennomførOverføringService,
-    internal val overføringRepository: OverføringRepository,
-    internal val overføringService: OverføringService,
-    internal val aleneOmOmsorgenRepository: AleneOmOmsorgenRepository,
-    internal val aleneOmOmsorgenService: AleneOmOmsorgenService,
-    internal val kafkaProducer: KafkaProducer<String, String>,
-    internal val formidlingService: FormidlingService,
-    internal val saksnummerRepository: SaksnummerRepository,
-    internal val saksnummerService: SaksnummerService,
-    internal val dataSource: DataSource,
-    internal val healthService: HealthService) {
-
-    internal fun start() {
-        dataSource.migrate()
-    }
-    internal fun stop() {
-        kafkaProducer.close()
-    }
-
-    internal class Builder(
-        internal var env: Environment? = null,
-        internal var accessTokenClient: AccessTokenClient? = null,
-        internal var omsorgspengerInfotrygdRammevedtakGateway: OmsorgspengerInfotrygdRammevedtakGateway? = null,
-        internal var infotrygdRammeService: InfotrygdRammeService? = null,
-        internal var fordelingService: FordelingService? = null,
-        internal var utvidetRettService: UtvidetRettService? = null,
-        internal var midlertidigAleneService: MidlertidigAleneService? = null,
-        internal var gjennomførOverføringService: GjennomførOverføringService? = null,
-        internal var overføringRepository: OverføringRepository? = null,
-        internal var overføringService: OverføringService? = null,
-        internal var aleneOmOmsorgenRepository: AleneOmOmsorgenRepository? = null,
-        internal var aleneOmOmsorgenService: AleneOmOmsorgenService? = null,
-        internal var kafkaProducer: KafkaProducer<String, String>? = null,
-        internal var formidlingService: FormidlingService? = null,
-        internal var saksnummerRepository: SaksnummerRepository? = null,
-        internal var saksnummerService: SaksnummerService? = null,
-        internal var dataSource: DataSource? = null) {
-        internal fun build() : ApplicationContext {
-            val benyttetEnv = env?:System.getenv()
-            val benyttetAccessTokenClient = accessTokenClient?:ClientSecretAccessTokenClient(
-                clientId = benyttetEnv.hentRequiredEnv("AZURE_APP_CLIENT_ID"),
-                clientSecret = benyttetEnv.hentRequiredEnv("AZURE_APP_CLIENT_SECRET"),
-                tokenEndpoint = URI(benyttetEnv.hentRequiredEnv("AZURE_APP_TOKEN_ENDPOINT"))
-            )
-            val benyttetOmsorgspengerInfotrygdRammevedtakGateway = omsorgspengerInfotrygdRammevedtakGateway?:OmsorgspengerInfotrygdRammevedtakGateway(
-                accessTokenClient = benyttetAccessTokenClient,
-                hentRammevedtakFraInfotrygdScopes = benyttetEnv.hentRequiredEnv("HENT_RAMMEVEDTAK_FRA_INFOTRYGD_SCOPES").csvTilSet(),
-                omsorgspengerInfotrygdRammevedtakBaseUrl = URI(benyttetEnv.hentRequiredEnv("OMSORGSPENGER_INFOTRYGD_RAMMEVEDTAK_BASE_URL"))
-            )
-            val benyttetInfotrygdRammeService = infotrygdRammeService?:InfotrygdRammeService(
-                omsorgspengerInfotrygdRammevedtakGateway = benyttetOmsorgspengerInfotrygdRammevedtakGateway
-            )
-
-            val benyttetKafkaProducer =  kafkaProducer ?: benyttetEnv.kafkaProducer()
-
-            val benyttetDataSource = dataSource ?: DataSourceBuilder(benyttetEnv).build()
-
-            val benyttetOverføringRepository = overføringRepository ?: OverføringRepository(
-                dataSource = benyttetDataSource
-            )
-
-            val benyttetAleneOmOmsorgenRepository = aleneOmOmsorgenRepository ?: AleneOmOmsorgenRepository(
-                dataSource = benyttetDataSource
-            )
-
-            val benyttetSaksnummerRepository = saksnummerRepository ?: SaksnummerRepository(
-                dataSource = benyttetDataSource
-            )
-
-            val benyttetSaksnummerService = saksnummerService ?: SaksnummerService(
-                saksnummerRepository = benyttetSaksnummerRepository
-            )
-
-            return ApplicationContext(
-                env = benyttetEnv,
-                accessTokenClient = benyttetAccessTokenClient,
-                omsorgspengerInfotrygdRammevedtakGateway = benyttetOmsorgspengerInfotrygdRammevedtakGateway,
-                infotrygdRammeService = benyttetInfotrygdRammeService,
-                fordelingService = fordelingService ?: FordelingService(
-                    infotrygdRammeService = benyttetInfotrygdRammeService
-                ),
-                utvidetRettService = utvidetRettService ?: UtvidetRettService(
-                    infotrygdRammeService = benyttetInfotrygdRammeService
-                ),
-                midlertidigAleneService = midlertidigAleneService ?: MidlertidigAleneService(
-                    infotrygdRammeService = benyttetInfotrygdRammeService
-                ),
-                gjennomførOverføringService = gjennomførOverføringService ?: GjennomførOverføringService(
-                    overføringRepository = benyttetOverføringRepository
-                ),
-                aleneOmOmsorgenRepository = benyttetAleneOmOmsorgenRepository,
-                aleneOmOmsorgenService = aleneOmOmsorgenService ?: AleneOmOmsorgenService(
-                    infotrygdRammeService = benyttetInfotrygdRammeService,
-                    aleneOmOmsorgenRepository = benyttetAleneOmOmsorgenRepository,
-                    saksnummerService = benyttetSaksnummerService
-                ),
-                healthService = HealthService(healthChecks = setOf(
-                    benyttetOmsorgspengerInfotrygdRammevedtakGateway
-                )),
-                kafkaProducer = benyttetKafkaProducer,
-                formidlingService = formidlingService ?: FormidlingService(
-                    kafkaProducer = benyttetKafkaProducer
-                ),
-                dataSource = benyttetDataSource,
-                overføringRepository = benyttetOverføringRepository,
-                overføringService = overføringService ?: OverføringService(
-                    infotrygdRammeService = benyttetInfotrygdRammeService,
-                    saksnummerService = benyttetSaksnummerService,
-                    overføringRepository = benyttetOverføringRepository
-                ),
-                saksnummerRepository = benyttetSaksnummerRepository,
-                saksnummerService = benyttetSaksnummerService
-            )
         }
     }
 }
---
+++ src/main/kotlin/no/nav/omsorgspenger/ApplicationContext.kt
@@ -0,0 +1,150 @@
+package no.nav.omsorgspenger
+
+import no.nav.helse.dusseldorf.ktor.health.HealthService
+import no.nav.helse.dusseldorf.oauth2.client.AccessTokenClient
+import no.nav.helse.dusseldorf.oauth2.client.ClientSecretAccessTokenClient
+import no.nav.k9.rapid.river.Environment
+import no.nav.k9.rapid.river.KafkaBuilder.kafkaProducer
+import no.nav.k9.rapid.river.csvTilSet
+import no.nav.k9.rapid.river.hentRequiredEnv
+import no.nav.omsorgspenger.aleneom.AleneOmOmsorgenRepository
+import no.nav.omsorgspenger.aleneom.AleneOmOmsorgenService
+import no.nav.omsorgspenger.fordelinger.FordelingService
+import no.nav.omsorgspenger.formidling.FormidlingService
+import no.nav.omsorgspenger.infotrygd.InfotrygdRammeService
+import no.nav.omsorgspenger.infotrygd.OmsorgspengerInfotrygdRammevedtakGateway
+import no.nav.omsorgspenger.midlertidigalene.MidlertidigAleneService
+import no.nav.omsorgspenger.overføringer.GjennomførOverføringService
+import no.nav.omsorgspenger.overføringer.OverføringRepository
+import no.nav.omsorgspenger.overføringer.OverføringService
+import no.nav.omsorgspenger.saksnummer.SaksnummerRepository
+import no.nav.omsorgspenger.saksnummer.SaksnummerService
+import no.nav.omsorgspenger.utvidetrett.UtvidetRettService
+import org.apache.kafka.clients.producer.KafkaProducer
+import java.net.URI
+import javax.sql.DataSource
+
+internal class ApplicationContext(
+    internal val env: Environment,
+    internal val accessTokenClient: AccessTokenClient,
+    internal val omsorgspengerInfotrygdRammevedtakGateway: OmsorgspengerInfotrygdRammevedtakGateway,
+    internal val infotrygdRammeService: InfotrygdRammeService,
+    internal val fordelingService: FordelingService,
+    internal val utvidetRettService: UtvidetRettService,
+    internal val midlertidigAleneService: MidlertidigAleneService,
+    internal val gjennomførOverføringService: GjennomførOverføringService,
+    internal val overføringRepository: OverføringRepository,
+    internal val overføringService: OverføringService,
+    internal val aleneOmOmsorgenRepository: AleneOmOmsorgenRepository,
+    internal val aleneOmOmsorgenService: AleneOmOmsorgenService,
+    internal val kafkaProducer: KafkaProducer<String, String>,
+    internal val formidlingService: FormidlingService,
+    internal val saksnummerRepository: SaksnummerRepository,
+    internal val saksnummerService: SaksnummerService,
+    internal val dataSource: DataSource,
+    internal val healthService: HealthService) {
+
+    internal fun start() {
+        dataSource.migrate()
+    }
+    internal fun stop() {
+        kafkaProducer.close()
+    }
+
+    internal class Builder(
+        internal var env: Environment? = null,
+        internal var accessTokenClient: AccessTokenClient? = null,
+        internal var omsorgspengerInfotrygdRammevedtakGateway: OmsorgspengerInfotrygdRammevedtakGateway? = null,
+        internal var infotrygdRammeService: InfotrygdRammeService? = null,
+        internal var fordelingService: FordelingService? = null,
+        internal var utvidetRettService: UtvidetRettService? = null,
+        internal var midlertidigAleneService: MidlertidigAleneService? = null,
+        internal var gjennomførOverføringService: GjennomførOverføringService? = null,
+        internal var overføringRepository: OverføringRepository? = null,
+        internal var overføringService: OverføringService? = null,
+        internal var aleneOmOmsorgenRepository: AleneOmOmsorgenRepository? = null,
+        internal var aleneOmOmsorgenService: AleneOmOmsorgenService? = null,
+        internal var kafkaProducer: KafkaProducer<String, String>? = null,
+        internal var formidlingService: FormidlingService? = null,
+        internal var saksnummerRepository: SaksnummerRepository? = null,
+        internal var saksnummerService: SaksnummerService? = null,
+        internal var dataSource: DataSource? = null) {
+        internal fun build() : ApplicationContext {
+            val benyttetEnv = env?:System.getenv()
+            val benyttetAccessTokenClient = accessTokenClient?: ClientSecretAccessTokenClient(
+                clientId = benyttetEnv.hentRequiredEnv("AZURE_APP_CLIENT_ID"),
+                clientSecret = benyttetEnv.hentRequiredEnv("AZURE_APP_CLIENT_SECRET"),
+                tokenEndpoint = URI(benyttetEnv.hentRequiredEnv("AZURE_APP_TOKEN_ENDPOINT"))
+            )
+            val benyttetOmsorgspengerInfotrygdRammevedtakGateway = omsorgspengerInfotrygdRammevedtakGateway?: OmsorgspengerInfotrygdRammevedtakGateway(
+                accessTokenClient = benyttetAccessTokenClient,
+                hentRammevedtakFraInfotrygdScopes = benyttetEnv.hentRequiredEnv("HENT_RAMMEVEDTAK_FRA_INFOTRYGD_SCOPES").csvTilSet(),
+                omsorgspengerInfotrygdRammevedtakBaseUrl = URI(benyttetEnv.hentRequiredEnv("OMSORGSPENGER_INFOTRYGD_RAMMEVEDTAK_BASE_URL"))
+            )
+            val benyttetInfotrygdRammeService = infotrygdRammeService?: InfotrygdRammeService(
+                omsorgspengerInfotrygdRammevedtakGateway = benyttetOmsorgspengerInfotrygdRammevedtakGateway
+            )
+
+            val benyttetKafkaProducer =  kafkaProducer ?: benyttetEnv.kafkaProducer()
+
+            val benyttetDataSource = dataSource ?: DataSourceBuilder(benyttetEnv).build()
+
+            val benyttetOverføringRepository = overføringRepository ?: OverføringRepository(
+                dataSource = benyttetDataSource
+            )
+
+            val benyttetAleneOmOmsorgenRepository = aleneOmOmsorgenRepository ?: AleneOmOmsorgenRepository(
+                dataSource = benyttetDataSource
+            )
+
+            val benyttetSaksnummerRepository = saksnummerRepository ?: SaksnummerRepository(
+                dataSource = benyttetDataSource
+            )
+
+            val benyttetSaksnummerService = saksnummerService ?: SaksnummerService(
+                saksnummerRepository = benyttetSaksnummerRepository
+            )
+
+            return ApplicationContext(
+                env = benyttetEnv,
+                accessTokenClient = benyttetAccessTokenClient,
+                omsorgspengerInfotrygdRammevedtakGateway = benyttetOmsorgspengerInfotrygdRammevedtakGateway,
+                infotrygdRammeService = benyttetInfotrygdRammeService,
+                fordelingService = fordelingService ?: FordelingService(
+                    infotrygdRammeService = benyttetInfotrygdRammeService
+                ),
+                utvidetRettService = utvidetRettService ?: UtvidetRettService(
+                    infotrygdRammeService = benyttetInfotrygdRammeService
+                ),
+                midlertidigAleneService = midlertidigAleneService ?: MidlertidigAleneService(
+                    infotrygdRammeService = benyttetInfotrygdRammeService
+                ),
+                gjennomførOverføringService = gjennomførOverføringService ?: GjennomførOverføringService(
+                    overføringRepository = benyttetOverføringRepository
+                ),
+                aleneOmOmsorgenRepository = benyttetAleneOmOmsorgenRepository,
+                aleneOmOmsorgenService = aleneOmOmsorgenService ?: AleneOmOmsorgenService(
+                    infotrygdRammeService = benyttetInfotrygdRammeService,
+                    aleneOmOmsorgenRepository = benyttetAleneOmOmsorgenRepository,
+                    saksnummerService = benyttetSaksnummerService
+                ),
+                healthService = HealthService(healthChecks = setOf(
+                    benyttetOmsorgspengerInfotrygdRammevedtakGateway
+                )),
+                kafkaProducer = benyttetKafkaProducer,
+                formidlingService = formidlingService ?: FormidlingService(
+                    kafkaProducer = benyttetKafkaProducer
+                ),
+                dataSource = benyttetDataSource,
+                overføringRepository = benyttetOverføringRepository,
+                overføringService = overføringService ?: OverføringService(
+                    infotrygdRammeService = benyttetInfotrygdRammeService,
+                    saksnummerService = benyttetSaksnummerService,
+                    overføringRepository = benyttetOverføringRepository
+                ),
+                saksnummerRepository = benyttetSaksnummerRepository,
+                saksnummerService = benyttetSaksnummerService
+            )
+        }
+    }
+}
