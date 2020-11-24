59d818c04a563098eed1edbd95fc7111b71f5bac
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-11-24 15:36:05 +0100

    Reformaterte funksjonen for å hente sisteDato

--- src/main/kotlin/no/nav/omsorgspenger/overføringer/Barn.kt
+++ src/main/kotlin/no/nav/omsorgspenger/overføringer/Barn.kt
@@ -24,16 +24,16 @@ internal data class Barn(
     )
 
     internal companion object {
-         internal fun JsonNode.somBarn() = Barn(
+        internal fun JsonNode.somBarn() = Barn(
             identitetsnummer = get("identitetsnummer").asText(),
             fødselsdato = LocalDate.parse(get("fødselsdato").asText()),
             aleneOmOmsorgen = get("aleneOmOmsorgen").asBoolean(),
             utvidetRett = get("utvidetRett").asBoolean()
         )
+
         internal fun List<Barn>.sisteDatoMedOmsorgenForOgAleneOmOmsorgen() =
-            filter { it.aleneOmOmsorgen }
-            .maxByOrNull { it.omsorgenFor.tom }?.let { barn ->
-            barn.omsorgenFor.tom to barn.utvidetRett
-        }
+            filter { it.aleneOmOmsorgen }.maxByOrNull { it.omsorgenFor.tom }?.let { barn ->
+                barn.omsorgenFor.tom to barn.utvidetRett
+            }
     }
 }
